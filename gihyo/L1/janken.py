49b264711ec907c7b43ffc73a146e9827b52fce7
Shogo Matsumoto <shogo@scarlet.shogo>
2018-05-22 02:11:24 +0900

    init (add L1, L2)

---
+++ gihyo/L1/janken.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+
+import random
+
+data = ["goo", "choki", "pa"]
+data_choice = random.choice(data)
+
+print(data_choice)
---
+++ gihyo/L2/functionExample.py
@@ -0,0 +1,30 @@
+#encoding:utf-8
+
+#----------------------
+# range function
+#----------------------
+intList1 = range(10)
+intList2 = range(1, 10, 2)
+
+print(intList1)
+print(intList2)
+
+
+#----------------------
+# split method 
+#----------------------
+string1 = '1st,2st,3st'
+charList = string1.split(',')
+
+print(charList)
+
+
+#----------------------
+# upper method 
+#----------------------
+string2 = 'apple'
+capital = string2.upper()
+
+print(capital)
+
+
